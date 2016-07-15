47a63330566b56cd58d719e567e7af4f43db0194
Zachery Hostens <zacheryph@gmail.com>
2016-07-15 15:32:43 -0500

    add short doc and example

--- doc.go
+++ doc.go
@@ -1,1 +1,6 @@
+/*
+	Package cmd replicates the command / flag management of the go tool
+	with minor modifications.  Some was ignored to prevent having to
+	pull in too much.
+*/
 package cmd
---
+++ example_test.go
@@ -0,0 +1,51 @@
+package cmd_test
+
+import "zro.net/go/cmd"
+
+var cmdGet = cmd.Command{
+	UsageLine: "get [-d] ...",
+	Short:     "compile packages and dependencies",
+	Long: `
+Get downloads go packages...
+
+The -d flag instructs get...
+				`,
+}
+
+var helpPath = cmd.Command{
+	UsageLine: "gopath",
+	Short:     "GOPATH environment variable",
+	Long: `
+The Go path is ...
+
+Lots of help stuff here.
+				`,
+}
+
+var flagD = cmdGet.Flag.String("d", "", "")
+
+func init() {
+	// This prevents flag/command compile dependency loop
+	cmdGet.Run = runGet
+
+	// register our commands
+	cmd.Register(&cmdGet)
+	cmd.Register(&helpPath)
+}
+
+func runGet(c *cmd.Command, args []string) {
+	// Do Stuff
+}
+
+// Example of command and help
+func main() {
+	// Parse / Execute our command
+	cmd.Parse()
+	// Nothing else to do here.
+}
+
+func Example() {
+	// Calling cmd.Parse() from main() processes/executes your command
+	// It will return though there wont be anything left for you to do
+	// as far as executing your command goes
+}
